ae05880e3ec4a8b0a7d2c1ffe7d9e028affaa2f2
Sara Morales Caravaca <56777997+milliways-png@users.noreply.github.com>
2026-08-08 20:28:27 -0600

    Feature shortcuts (#2)
    * the user can add shortcuts to the corresponding directory and the app renders them
    
    * Intermediate commit
    
    * Fix styles
    
    * add settings menu layout

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,82 @@
+use tauri::Manager;
+use std::fs;
+use std::process::Command as SystemCommand; //alias to avoid clashes with tauri
+
+// command to scan shortcuts dir and return a list of the filenames
+#[tauri::command]
+fn get_shortcuts(app: tauri::AppHandle) -> Vec<String> {
+  let mut files = Vec::new();
+
+  if let Some(doc_dir) = app.path().document_dir().ok() {
+    let target_dir = doc_dir.join("cozy_launcher_shortcuts");
+
+    // read items in dir
+    if let Ok(entries) = fs::read_dir(target_dir) {
+      for entry in entries.flatten() {
+        if let Ok(file_type) = entry.file_type() {
+          if file_type.is_file() {
+            let filename = entry.file_name().to_string_lossy().into_owned();
+            files.push(filename);
+          }
+        }
+      }
+    }
+  }
+  files // returns vector of filenames
+}
+
+
+// command to execute a chosen shortcut when clicked
+#[tauri::command]
+fn launch_shortcut(app: tauri::AppHandle, filename: String) -> Result<(), String> {
+    if let Some(doc_dir) = app.path().document_dir().ok() {
+        let shortcut_path = doc_dir.join("cozy_launcher_shortcuts").join(&filename);
+        #[cfg(target_os = "windows")]
+        {
+            // On Windows, use the native cmd shell wrapper to execute shortcuts (.lnk / .url / .exe)
+            SystemCommand::new("cmd")
+                .args(["/C", "start", "", &shortcut_path.to_string_lossy()])
+                .spawn()
+                .map_err(|e| e.to_string())?;
+        }
+
+        #[cfg(target_os = "macos")]
+        {
+            // On macOS, use the native 'open' tool to run application shortcuts or scripts
+            SystemCommand::new("open")
+                .arg(&shortcut_path)
+                .spawn()
+                .map_err(|e| e.to_string())?;
+        }
+
+        #[cfg(target_os = "linux")]
+        {
+            // On Linux, use xdg-open to trigger desktop shortcut files (.desktop)
+            SystemCommand::new("xdg-open")
+                .arg(&shortcut_path)
+                .spawn()
+                .map_err(|e| e.to_string())?;
+        }
+
+        return Ok(());
+    }
+    Err("Could not locate documents path".into())
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
+    //NOTE: register commands so that angular can read them
+    .invoke_handler(tauri::generate_handler![get_shortcuts, launch_shortcut]) 
     .setup(|app| {
+      // NOTE: create shortcuts directory
+      if let Some(doc_dir) = app.path().document_dir().ok() {
+        let target_dir = doc_dir.join("cozy_launcher_shortcuts");
+        match fs::create_dir_all(&target_dir) {
+          Ok(_) => println!("created dir successfully! at {:?}", target_dir), //TODO: change for 'match' for "let _ =" and remove Ok and Error cases
+          Err(e)=> eprintln!("failed to create dir {}", e),
+        }
+      }
       if cfg!(debug_assertions) {
         app.handle().plugin(
           tauri_plugin_log::Builder::default()
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,44 @@
-import { Component, signal, NgZone } from '@angular/core';
+import { Component, signal, NgZone, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { invoke } from '@tauri-apps/api/core';
 import { RouterOutlet } from '@angular/router';
 import { getCurrentWindow } from '@tauri-apps/api/window'
+import { Config } from './features/config/config';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, Config],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+  shortcutFiles: string[] = [];
+  showConfig: boolean = false;
+  protected readonly title = signal('cozy-launcher');
   constructor(private ngZone: NgZone) {}
 
-  protected readonly title = signal('cozy-launcher');
-  
+  ngOnInit(): void {
+    this.loadShortcuts();
+  }
+
+  // NOTE: Ask Rust for the list of files inside "my_app_data"
+  async loadShortcuts() {
+    try {
+      this.shortcutFiles = await invoke<string[]>('get_shortcuts');      
+    } catch (error) {
+      console.log('could not read shortcuts folder', error);
+    }
+  }
+
+  // NOTE: Tell Rust to launch the file that was clicked
+  async openShortcut(filename: string) {
+    try {
+      await invoke('launch_shortcut', { filename });
+    } catch (error) {
+      console.error('could not execute ${filename}:', error);
+    }
+  }
+
   onWindowDrag(event: MouseEvent) {
     // Only drag if the user clicks the left mouse button
     if (event.button === 0) {
